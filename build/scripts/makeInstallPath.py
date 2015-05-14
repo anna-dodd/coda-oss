370f49ef2f601eea0b2f6ed59ae1c510cb92ce2e
anna-dodd <anna.dodd@mdaus.com>
2015-05-14 10:45:26 -0400

    Update makeInstallPath.py

--- build/scripts/makeInstallPath.py
+++ build/scripts/makeInstallPath.py
@@ -24,5 +24,5 @@ def installPath(package_name):
     
     install_path = "%s-%s-r%s" % (package_name,install_suffix,os.environ.get('GIT_COMMIT', ''))
     #install_path = "%s-%s-r%s" % (package_name,install_suffix,os.environ.get('SVN_REVISION', ''))
-    install_path = join('..', install_path)
+    #install_path = join('..', install_path)
     return install_path
